3653bee47bdbc3f49e5d05060e5322695285eb02
fuzzbin <tom.jarle.christiansen@telemarkfylke.no>
2025-10-23 21:42:01 +0200

    feat: enhance Mistral OCR processing with dynamic form schema and base64 PDF support

--- src/functions/mistralOCR.js
+++ src/functions/mistralOCR.js
@@ -12,11 +12,30 @@ app.http('mistralOCR', {
             apiKey: process.env.MISTRAL_API_KEY
         });
 
+        // ToDo Lage ulike schema for ulike dokumenttyper slik at disse kan velges dynamisk basert på input
         const ImageSchema = z.object({
             image_type: z.string(),
             short_description: z.string(),
             summary: z.string(),
         });
+
+        const formSchema = z.object({
+            referanseid: z.string().optional(),
+            skjemaid: z.string().optional(),
+            fornavn: z.string().optional(),
+            etternavn: z.string().optional(),
+            adresse: z.string().optional(),
+            postnummer: z.string().optional(),
+            poststed: z.string().optional(),
+            telefonnummer: z.string().optional(),
+            epost: z.string().optional(),
+            fodselsdato: z.string().describe("De seks første sifferne i fødselsnummeret. Formatet er DDMMYYxxxxx").optional(),
+            fodselsnummer: z.string().describe("11 siffer"),
+            bekreftelse: z.boolean().describe("Om sjekkboksen er merket").optional(),
+        })
+
+        const body = await request.json();
+        const base64Pdf = body.base64Pdf;
 
         const result = await mistral.ocr.process({
         model: "mistral-ocr-latest",
@@ -24,9 +43,10 @@ app.http('mistralOCR', {
         // documentAnnotationFormat: "json_object",
         // pages: [1],
         bboxAnnotationFormat: responseFormatFromZodObject(ImageSchema),
+        documentAnnotationFormat: responseFormatFromZodObject(formSchema),
         document: {
-            documentUrl: "https://raw.githubusercontent.com/mistralai/cookbook/refs/heads/main/mistral/ocr/mistral7b.pdf",
             type: "document_url",
+            documentUrl: "data:application/pdf;base64," + base64Pdf
         },
         });
         console.log(result)
@@ -62,16 +82,31 @@ app.http('questions', {
         const mistral = new Mistral({
             apiKey: process.env.MISTRAL_API_KEY,
         });
+        console.log("Processing questions...")
 
-        const result = await mistral.ocr.process({
-        model: "mistral-ocr-latest",
-        includeImageBase64: true,
-        documentAnnotationFormat: "json_object",
-        document: {
-            documentUrl: "https://raw.githubusercontent.com/mistralai/cookbook/refs/heads/main/mistral/ocr/mistral7b.pdf",
-            type: "document_url",
-        },
+        const body = await request.json();
+        const base64Pdf = body.base64Pdf;
+        console.log("Received PDF of length: " + base64Pdf.length)
+        const result = await mistral.chat.complete({
+            model: "mistral-small-latest",
+            messages: [
+                {
+                    role: "user",
+                    content: [
+                        {
+                            type: "text",
+                            text: "What is the last sentence in the document?",
+                        },
+                        {
+                            type: "document_url",
+                            documentUrl: "data:application/pdf;base64," + base64Pdf,
+                        },
+                    ],
+                },
+            ],
         });
+        console.log(result)
+        return {status: result.status, jsonBody: result}
     }
 })
 
